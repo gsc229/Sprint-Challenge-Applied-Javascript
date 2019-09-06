5e1d34161884134e54d726aacd40acb14e518c10
Greg <gsc229@gmail.com>
2019-09-06 12:13:50 -0400

    heading and topics components added

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,26 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const headOne = Header("Sept 6, 2019", "Lambda Times", "98");
+const headerContainer = document.querySelector(".header-container");
+headerContainer.appendChild(headOne);
+function Header(d, ti, tem) {
+  const header = document.createElement("div");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temp = document.createElement("span");
+  // append items appropriately:
+  header.appendChild(date);
+  header.appendChild(title);
+  header.appendChild(temp);
+  // add the classes
+  header.classList.add("header");
+  date.classList.add("date");
+  temp.classList.add("temp");
+  // add the content to the elements:
+  date.textContent = d;
+  title.textContent = ti;
+  temp.textContent = `${tem}deg`;
 
-function Header() {}
+  return header;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,27 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+let topicsArray = [];
+const topicsSection = document.querySelector(".topics");
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    topicsArray = response.data.topics;
+    topicsArray.forEach(topic => {
+      topicsSection.appendChild(topicCreator(topic));
+    });
+    console.log(topicsArray);
+  })
+
+  .catch(error1 => {
+    console.log("The data was not returned", error1);
+  });
+
+// crate the tab component function function:
+function topicCreator(topic) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = topic;
+
+  return tab;
+}
